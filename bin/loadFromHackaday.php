11bc489c4fbcd899b91c0d42fe405fb47edc0fd0
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-28 20:20:19 +0100

    Extraction done. Removed unused files.

--- bin/loadFromHackaday.php
+++ bin/loadFromHackaday.php
@@ -1,5 +1,14 @@
 <?php
 
+$api_key = "aA99r87lSTtkLyRL";
+$project_id = "9914";
 
+$url = "https://api.hackaday.io/v1/projects/".$project_id."/logs?api_key=".$api_key;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_ENCODING, "");
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_exec($ch);
 
 ?>
