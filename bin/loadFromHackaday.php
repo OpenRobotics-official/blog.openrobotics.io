be14eabff2d77029ade6ff08986432d95150fea2
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-28 20:26:04 +0100

    Removed headers from answer.

--- bin/loadFromHackaday.php
+++ bin/loadFromHackaday.php
@@ -8,7 +8,7 @@ $url = "https://api.hackaday.io/v1/projects/".$project_id."/logs?api_key=".$api_
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_ENCODING, "");
-curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_exec($ch);
 
 ?>
