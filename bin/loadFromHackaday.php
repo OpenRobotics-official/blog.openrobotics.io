2946150c83f433bba7de5fd001406089ba758592
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-28 22:53:49 +0100

    Added .html and .css. Issue include file.php

--- bin/loadFromHackaday.php
+++ bin/loadFromHackaday.php
@@ -1,14 +1,41 @@
 <?php
 
+/**
+ * Ce script a pour but d'aller récupérer via l'API de Hackaday les posts de OpenRobotics afin de les afficher sur notre site web.
+ * @author	Cyrille Benoit <cyrillebenoitpro@gmail.com>
+ * @copyright	© 2016, OpenRobotics
+ */
+
 $api_key = "aA99r87lSTtkLyRL";
 $project_id = "9914";
 
-$url = "https://api.hackaday.io/v1/projects/".$project_id."/logs?api_key=".$api_key;
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_ENCODING, "");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
+//Récupération des logs du projet au format JSON via cURL
+{
+	$url = "https://api.hackaday.io/v1/projects/".$project_id."/logs?api_key=".$api_key;
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_ENCODING, "");
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$html = curl_exec($ch);
+	curl_close($ch);
+	//Prise en charge des caractères accentués et suppression des balises HTML (<p> </p> du body notemment)
+	$html = htmlspecialchars_decode($html);
+	$html = html_entity_decode($html);
+	$html = strip_tags($html);
+
+	$dataJson = json_decode($html, true);
+	unset($url, $html, $ch);
+}
+
+print_r($dataJson);
+
+if(!empty($dataJson['logs'])) {
+	foreach ($dataJson['logs'] as $log) {
+	print("<class=\"HADTimestamp\">".$log['created']."</class><class=\"HADTitle\">".$log['title']."</class>\n<class = \"HADBody\">".$log['body']."</class>\n");
+	}
+}
 
 ?>
